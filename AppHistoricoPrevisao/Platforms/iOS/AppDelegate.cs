61699cc5314ec88fae0c952358e3e90f29ad6308
Usuario <Usuario@CPD03-M16>
2024-10-17 15:06:27 -0300

    Adicionar arquivos de projeto.

---
+++ AppHistoricoPrevisao/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppHistoricoPrevisao
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ AppHistoricoPrevisao/Views/HistoricoClima.xaml.cs
@@ -0,0 +1,9 @@
+namespace AppHistoricoPrevisao.Views;
+
+public partial class HistoricoClima : ContentPage
+{
+	public HistoricoClima()
+	{
+		InitializeComponent();
+	}
+}
